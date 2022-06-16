7af7e5aac02f1a0dffc2432f9911537171688d3c
Mark Q Schwanda <mqschwanda@gmail.com>
2022-06-16 06:59:36 -0400

    feature(eslint): add eslint config and JS boilerplate

---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+// eslint-disable-next-line no-console
+console.log('lint me by running `yarn lint` from your command line');
+
+console.log('this line should throw an error for missing semi...') // eslint-disable-line no-console
